291d3bb0bea282bb7fc8d4bca2d1681551781c77
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-05-02 17:15:08 +0500

    result slider desktop in progress

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let swiper = new Swiper(".resultSwiper", {
+    slidesPerView: 1.50,
+    centeredSlides: true,
+    // spaceBetween: 20,
+    // breakpoints: {
+    //     320: {
+    //       slidesPerView: 1,
+    //       spaceBetween: 20,
+    //     },
+    // }
+});
