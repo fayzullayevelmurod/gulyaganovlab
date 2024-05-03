256b0e3b37a40b9eadfa6fda9fab9eecee48ca55
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-05-03 23:54:30 +0500

    media the end

--- js/main.js
+++ js/main.js
@@ -1,33 +1,43 @@
 let swiper = new Swiper(".resultSwiper", {
-    slidesPerView: 3.10,
+    spaceBetween: 10,
     // centeredSlides: true,
-    spaceBetween: 24,
 
     navigation: {
         nextEl: ".result-button-next",
         prevEl: ".result-button-prev",
     },
-    // breakpoints: {
-    //     320: {
-    //       slidesPerView: 1,
-    //       spaceBetween: 20,
-    //     },
-    // }
+    breakpoints: {
+        576: {
+          slidesPerView: 1.15,
+          spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2.10,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 3.10,
+            spaceBetween: 24,
+        },
+    }
 });
 
 let swiper2 = new Swiper(".teacherSwiper", {
-    slidesPerView: 4,
-    // centeredSlides: true,
     spaceBetween: 15,
-
+    // centeredSlides: true,
     navigation: {
         nextEl: ".teacher-button-next",
         prevEl: ".teacher-button-prev",
     },
-    // breakpoints: {
-    //     320: {
-    //       slidesPerView: 1,
-    //       spaceBetween: 20,
-    //     },
-    // }
+    breakpoints: {
+        576: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1200: {
+            slidesPerView: 4,
+        },
+    }
 });
